519b3331c8b44991fe0e5cb7ab3e8852bee14736
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-04-23 13:46:28 -0400

    initial commit

---
+++ src/Content.jsx
@@ -0,0 +1,13 @@
+import { ProductsIndex } from "./ProductsIndex";
+
+export function Content() {
+  const products = [
+    { id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150 },
+    { id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300 },
+  ];
+  return (
+    <div>
+      <ProductsIndex products={products} />
+    </div>
+  );
+}
---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,7 @@
+export function ProductsIndex() {
+  return (
+    <div>
+      <h1>All photos</h1>
+    </div>
+  );
+}
